56359e30d37b430b6078734e3cafdb2a5fdcb4c4
annvitra <annvitra.com@gmail.com>
2025-10-31 12:09:42 +0530

    fix: Update API base URL

--- src/services/apiService.jsx
+++ src/services/apiService.jsx
@@ -1,4 +1,4 @@
-const API_BASE_URL = '/api';
+const API_BASE_URL = 'http://localhost:24917/api';
 
 const handleResponse = async (response) => {
   const data = await response.json().catch(() => ({}));
